858eb4ad186f6da63d20372aa8efc10c56aed602
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2022-03-31 00:32:56 +0430

    Configure tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
   return (
     <div className="App">
       <InputButton />
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
